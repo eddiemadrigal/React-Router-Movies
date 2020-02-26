89bf081da8ff46dfbb815559992c0b42f05f6589
edmadrigal <edmadrigal@yahoo.com>
2020-02-25 17:23:16 -0800

    MovieCard component added to MovieDetails component

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
 
-const MovieCard = props => {
-  return;
-};
+const MovieCard = (props) => {
+  console.log("props: ", props.movie);
+  const params = useParams();
+  const [movie, setMovie] = useState();
+  const id = params.id;
+  console.log(id);
+
+  useEffect(() => {
+    setMovie(props.movie);
+  },[]);
+  
+  // Uncomment this only when you have moved on to the stretch goals
+  // const saveMovie = () => {
+  //   const addToSavedList = props.addToSavedList;
+  //   addToSavedList(movie)
+  // }
+
+  if (!movie) {
+    return <div>Loading movie information...</div>;
+  }
+
+  const { title, director, metascore, stars } = movie;
+  return (
+    <div className="save-wrapper">
+      <div className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </div>
+      <div className="save-button">Save</div>
+    </div>
+  );
+}
 
 export default MovieCard;
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import MovieCard from "./MovieCard";
 
 
 const MovieList = props => {
@@ -17,25 +18,9 @@ const MovieList = props => {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
-      <h2>Title: {title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
+    <MovieCard movie={movie} />
+  )
 }
 
 export default MovieList;
